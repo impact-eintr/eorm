3f28ec76965eea4494316d92747330c90ee30b3e
impactr-eintr <yixingwei4@gmail.com>
2022-07-26 11:59:04 +0800

    maven友好型目录结构

--- ibatis/src/main/java/org/apache/ibatis/reflection/Reflector.java
+++ ibatis/src/main/java/org/apache/ibatis/reflection/Reflector.java
@@ -1,4 +1,17 @@
 package org.apache.ibatis.reflection;
 
+import org.apache.ibatis.reflection.invoker.Invoker;
+
+import java.util.Map;
+
 public class Reflector {
+  // 要被反射解析的类
+  private final Class<?> type;
+
+  private final String[] readablePropertyNames;
+
+  private final String[] writablePropertyNames;
+
+  private final Map<String, Invoker>
+
 }
